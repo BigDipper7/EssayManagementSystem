b139ba22184aef3cab79a2c0e3c5293bc0cb7c62
Minghao Xu <violiniselegant@gmail.com>
2016-04-13 19:50:46 +0800

    add all needed files and architecture

---
+++ src/com/eva/me/controller/StudentController.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.eva.me.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.eva.me.model.Student;
+
+/**
+ * @author phoen_000
+ *
+ */
+@RequestMapping("/student")
+@Controller
+public class StudentController {
+	
+	@RequestMapping(value={""}, method=RequestMethod.GET)
+	public ModelAndView student() {
+		return new ModelAndView("student", "command", new Student());
+	}
+	
+	@RequestMapping(value={"/add"},method=RequestMethod.POST)
+	public String addStudent(@ModelAttribute("SpringWeb")Student student, ModelMap modelMap) {
+		modelMap.addAttribute("account",student.getAccount());
+		modelMap.addAttribute("name", student.getName());
+		modelMap.addAttribute("age", student.getAge());
+		return "Result";
+	}
+	
+}
---
+++ src/com/eva/me/model/Student.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.eva.me.model;
+
+/**
+ * @author phoen_000
+ *
+ */
+public class Student {
+	private String name;
+	private String age;
+	private int account;
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the age
+	 */
+	public String getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(String age) {
+		this.age = age;
+	}
+	/**
+	 * @return the account
+	 */
+	public int getAccount() {
+		return account;
+	}
+	/**
+	 * @param account the account to set
+	 */
+	public void setAccount(int account) {
+		this.account = account;
+	}
+}
