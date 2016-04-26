ef86070fb9d24eb3f3dc9bab7f9e9ca740f22a74
Minghao Xu <violiniselegant@gmail.com>
2016-04-26 17:15:09 +0800

    add model Essay

---
+++ src/com/eva/me/model/Essay.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.eva.me.model;
+
+/**
+ * @author phoen_000
+ *
+ */
+public class Essay {
+	public String essayContent;
+	public String essayTitle;
+	public String essayAuthor;
+	/**
+	 * @return the essayContent
+	 */
+	public String getEssayContent() {
+		return essayContent;
+	}
+	/**
+	 * @param essayContent the essayContent to set
+	 */
+	public void setEssayContent(String essayContent) {
+		this.essayContent = essayContent;
+	}
+	/**
+	 * @return the essayTitle
+	 */
+	public String getEssayTitle() {
+		return essayTitle;
+	}
+	/**
+	 * @param essayTitle the essayTitle to set
+	 */
+	public void setEssayTitle(String essayTitle) {
+		this.essayTitle = essayTitle;
+	}
+	/**
+	 * @return the essayAuthor
+	 */
+	public String getEssayAuthor() {
+		return essayAuthor;
+	}
+	/**
+	 * @param essayAuthor the essayAuthor to set
+	 */
+	public void setEssayAuthor(String essayAuthor) {
+		this.essayAuthor = essayAuthor;
+	}
+}
