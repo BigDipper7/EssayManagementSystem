83a91368be3628562656fc70d3b9431c335f0404
Minghao Xu <violiniselegant@gmail.com>
2016-05-05 17:01:01 +0800

    add DAO and Service impl

--- src/com/eva/me/dao/UserDao.java
+++ src/com/eva/me/dao/UserDao.java
@@ -3,10 +3,18 @@
  */
 package com.eva.me.dao;
 
+import java.util.List;
+
+import com.eva.me.model.User;
+
 /**
  * @author phoen_000
  *
  */
-public class UserDao {
-
+public interface UserDAO {
+	/**
+	 * list all users
+	 * @return
+	 */
+	public List<User> list(); 
 }
