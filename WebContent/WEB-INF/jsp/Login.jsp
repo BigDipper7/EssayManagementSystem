092bce53244310924e9b31f0aa80cefdf8688ae6
Minghao Xu <violiniselegant@gmail.com>
2016-05-12 14:17:01 +0800

    add error echo and add input password type

--- WebContent/WEB-INF/jsp/Login.jsp
+++ WebContent/WEB-INF/jsp/Login.jsp
@@ -1,16 +1,19 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
 <%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>Insert title here</title>
 </head>
 <body>
-	<c:if test="">
+	<c:if test="${ Errors!=null }">
+		<c:forEach items="${ Errors }" var="error">
+			<font color="red"><c:out value="${ error }"></c:out></font>
+		</c:forEach>
 	</c:if>
 	<h1>Login!</h1>
 	<form:form method="POST" action="${pageContext.request.contextPath}/login" commandName="User">
@@ -21,7 +24,7 @@
 			</tr>
 			<tr>
 				<td><form:label path="password">Password:</form:label></td>
-				<td><form:input path="password"/></td>
+				<td><form:password path="password"/></td>
 			</tr>
 			<tr>
 				<td><input type="submit" value="Login!"/></td>
--- src/com/eva/me/controller/UserController.java
+++ src/com/eva/me/controller/UserController.java
@@ -3,6 +3,9 @@
  */
 package com.eva.me.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,6 +15,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.eva.me.model.User;
 import com.eva.me.service.UserService;
+import com.eva.me.util.Log;
 
 /**
  * @author phoen_000
@@ -21,18 +25,31 @@ import com.eva.me.service.UserService;
 public class UserController {
 	
 	@RequestMapping(value="/login", method=RequestMethod.GET)
-	public ModelAndView showLoginPage() {
+	public String showLoginPage(ModelMap modelMap) {
+//		if (user == null) {
+//			Log.e("user is null~");
+//			user = new User();
+//		}
 		User user = new User();
-//		user.setUsername("this is new username");
-		return new ModelAndView("Login", "User", user);
+		user.setUsername("this is new username");
+//		return new ModelAndView("Login", "User", user);
+		modelMap.addAttribute("User",user);
+		return "Login";
 	}
 	
 	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public ModelAndView handleLoginAction(@ModelAttribute User user, ModelMap modelMap) {
+	public String handleLoginAction(@ModelAttribute User user, ModelMap modelMap) {
 		User resUser = new UserService().loginUser(user);
 		if (resUser == null) {
-//			return "redirect:login";
-			return new ModelAndView("Login", "User", new User());
+//			user.setUsername("Wrong!!");
+//			modelMap.addAttribute("User",user);
+			List<String> errorMsgs = new ArrayList<String>();
+			errorMsgs.add("µÇÂ½Ê§°Ü£¬ÓÃ»§Ãû»òÃÜÂë´íÎó£¡");
+			modelMap.addAttribute("Errors", errorMsgs);
+			user.setPassword("");
+			modelMap.addAttribute("User", user);
+			return "Login";
+//			return new ModelAndView("Login", "User", new User());
 		}
 		
 //		modelMap.addAttribute("id", resUser.getId());
@@ -40,7 +57,8 @@ public class UserController {
 //		modelMap.addAttribute("password", resUser.getPassword());
 //		modelMap.addAttribute("email", resUser.getEmail());
 		
-//		return "Main";
-		return new ModelAndView("Main", "user", resUser);
+		modelMap.addAttribute("user",resUser);
+		return "Main";
+//		return new ModelAndView("Main", "user", resUser);
 	}
 }
