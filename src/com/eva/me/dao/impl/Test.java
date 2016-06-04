9623d3c847228eca4a4382c2865c7f79b28709a8
Minghao Xu <violiniselegant@gmail.com>
2016-06-04 10:26:55 +0800

    add needed hierachey

---
+++ src/com/eva/me/dao/impl/Test.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.eva.me.dao.impl;
+
+/**
+ * @author violi
+ *
+ */
+public class Test {
+
+}
---
+++ src/com/eva/me/service/impl/Test.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.eva.me.service.impl;
+
+/**
+ * @author violi
+ *
+ */
+public class Test {
+
+}
