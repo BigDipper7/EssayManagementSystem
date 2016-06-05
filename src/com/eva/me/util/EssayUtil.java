6a9400422ef791f5be8df3abf55ec48be862edce
Minghao Xu <violiniselegant@gmail.com>
2016-06-05 16:07:31 +0800

    add util class,  decode html entity to  chinese word

--- src/com/eva/me/util/EssayUtil.java
+++ src/com/eva/me/util/EssayUtil.java
@@ -34,11 +34,15 @@ public class EssayUtil {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+
+		question = decode(question);
+		answer = decode(answer);
+		System.out.println("question:"+question+"ans:"+answer);
 		
-//		QAPair temp = new QAPair(question,answer);
-//		Gson gson = new Gson();
-//		String tempJson = gson.toJson(temp);
-		String tempJson = question+answer;
+		QAPair temp = new QAPair(question,answer);
+		Gson gson = new Gson();
+		String tempJson = gson.toJson(temp);
+//		String tempJson = question+answer;
 		Log.i("=============json: "+tempJson);
 		
 		final String fileName = md5(question+answer);
@@ -75,6 +79,55 @@ public class EssayUtil {
 	    }  
 	    return pwd;  
 	}
+	
+	
+	
+	/**
+	 * 可将中文转换成 "&#" 开头的html实体编码
+	 *
+	 *
+	 * @param str
+	 * @return
+	 */
+	public static String encode(String str) {
+	  char[] arrs = str.toCharArray();//Hex.encodeHex();
+	  StringBuilder sb = new StringBuilder();
+	  for (char c : arrs) {
+	    // \\u 表示Unicode编码。
+	    if (c >= '\u2E80' && c <= '\uFE4F') {//  [ 只是中文一般 [ \u4e00-\u9fa5]；中日韩统一表意文字（CJK Unified Ideographs） [\u2E80-\uFE4F]
+	      sb.append("&#").append((int)c).append(";");
+	    } else {
+	      sb.append(c);
+	    }
+	  }
+	  return sb.toString();
+	}
+
+
+	/**
+	 *  "&#" 开头的html实体编码 转换成中文（其实只是将长度为5的整型作了转换，对开其它如英文实体会出现错误。）
+	 *
+	 * <option value="zh_CN">&#20013;&#25991; (&#31616;&#20307;)</option><option value="zh_TW">&#20013;&#25991; (&#32321;&#39636;)</option>
+	 *
+	 * @param str
+	 * @return
+	 */
+	@Deprecated
+	public static String decode(String str) {
+	  String[] tmp = str.split(";&#|&#|;");
+	  StringBuilder sb = new StringBuilder("");
+	  for (int i = 0; i < tmp.length; i++) {
+	    if (tmp[i].matches("\\d{5}")) {
+	      sb.append((char) Integer.parseInt(tmp[i]));
+	    } else {
+	      sb.append(tmp[i]);
+	    }
+	  }
+	  return sb.toString();
+	}
+	
+	
+	
 	
 	public static void main(String[] args) {
 		Essay test = new EssayDAOImpl().getEssayById(8);
