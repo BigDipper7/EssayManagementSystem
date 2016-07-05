18e455c38f51385fcefd8888d2a1076fff459efb
Minghao Xu <violiniselegant@gmail.com>
2016-07-05 10:19:22 +0800

    add import script

---
+++ src/com/eva/me/util/ImportData.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.eva.me.util;
+
+/**
+ * @author violi
+ *
+ */
+public class ImportData {
+	public static void main(String[] args) {
+		final String string = "";
+		
+	}
+
+}
