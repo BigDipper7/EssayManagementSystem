0b4d7681a323be2ce3bc3e7b09171b457b997d01
Minghao Xu <violiniselegant@gmail.com>
2016-09-05 11:59:27 +0800

    add config files

--- src/com/eva/me/util/Config.java
+++ src/com/eva/me/util/Config.java
@@ -10,4 +10,10 @@ package com.eva.me.util;
 public class Config {
 	public static final String SESSION_KEY_USER = "auth_user";
 	public static final String SESSION_KEY_USER_IS_LOGIN = "auth_user_is_login";
+	
+	public static String FILE_PATH_FILE_UPLOAD = "";
+	public static String FILE_PATH_FILE_EXPORT = "";
+	public static String FILE_PATH_INDEX_BUILD = "";
+	public static String FILE_PATH_DOCUMENT_EXPORT = "";
+	
 }
